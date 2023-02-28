53d6bc93ed2674e63f43deb2d4c1f4950df51e19
singhi36 <singhi36@mymacewan.ca>
2023-02-27 20:13:36 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+   using System;
+namespace outOfPotential
+    {
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string inputS = "Hello inder";
+            int count = inputS.GetWordCount();
+
+            Console.WriteLine("String value "+inputS);
+            Console.WriteLine("Word count " + count);
+
+        }
+    }
+        public static class ExtensionMethods
+        {
+            public static int GetWordCount(this string someData)
+            {
+                string[] stringArray = someData.Split(' ');
+                int arrayLength = stringArray.Length;
+                return arrayLength;
+            }
+
+        }
+    }
